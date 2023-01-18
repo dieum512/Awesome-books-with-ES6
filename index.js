bcb90f1123cf9688f3b643ac0364e343304461d4
dieum512 <akonkwadieum222@gmail.com>
2023-01-18 12:06:05 +0200

    Put the modules in the module file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import Book from './book.js';
-import Store from './store.js';
-import UI from './UI.js';
+import Book from './modules/book.js';
+import Store from './modules/store.js';
+import UI from './modules/UI.js';
 
 // display books
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
